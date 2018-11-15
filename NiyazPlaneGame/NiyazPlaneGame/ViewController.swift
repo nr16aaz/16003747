ad697b0089e13529b781a72d025a1305a23d1e1e
nr16aaz <44379567+nr16aaz@users.noreply.github.com>
2018-11-15 16:04:37 +0000

    Add files via upload

---
+++ NiyazPlaneGame/NiyazPlaneGame/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  NiyazPlaneGame
+//
+//  Created by nr16aaz on 15/11/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var roadImage: UIImageView!
+    class road: UIViewController {
+    
+     override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    var imageArray: [UIImage]!
+    
+    imageArray = [UIImage(named: "road1.png")!,
+            UIImage(named: "road2.png")!,
+            UIImage(named: "road3.png")!,
+            UIImage(named: "road4.png")!,
+            UIImage(named: "road5.png")!,
+            UIImage(named: "road6.png")!,
+            UIImage(named: "road7.png")!,
+            UIImage(named: "road8.png")!,
+            UIImage(named: "road9.png")!,
+            UIImage(named: "road10.png")!,
+            UIImage(named: "road11.png")!,
+            UIImage(named: "road12.png")!,
+            UIImage(named: "road13.png")!,
+            UIImage(named: "road14.png")!,
+            UIImage(named: "road15.png")!,
+            UIImage(named: "road17.png")!,
+            UIImage(named: "road18.png")!,
+            UIImage(named: "road19.png")!]
+    
+    roadImage.image = UIImage.animatedImage(with: imageArray, duration: 0.3)
+    }
+
+}
+}
+
+
+
---
+++ NiyazPlaneGame/NiyazPlaneGame/dynamics.swift
@@ -0,0 +1,9 @@
+//
+//  dynamics.swift
+//  NiyazPlaneGame
+//
+//  Created by nr16aaz on 15/11/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
